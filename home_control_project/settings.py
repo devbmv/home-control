618243344b82ab41be39007990446e9792862744
devbmv <maik_775@yahoo.com>
2024-10-03 16:35:49 +0100

    Change env var collect instructions in settings.py

--- home_control_project/settings.py
+++ home_control_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 DEBUG = os.getenv("DEBUG", "False") == "False"
 
 # Allowed hosts configuration for both local and production environments
-ALLOWED_HOSTS = json.loads(os.getenv("ALLOWED_HOSTS", '["127.0.0.1"]'))
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "127.0.0.1").split(',')
 
 
 # API credentials loaded from environment variables
@@ -164,7 +164,7 @@ else:
 
 # CSRF trusted origins configuration for securing
 #  cross-site request forgery protection
-CSRF_TRUSTED_ORIGINS = json.loads(os.getenv("CSRF_TRUSTED_ORIGINS", '[]'))
+CSRF_TRUSTED_ORIGINS = os.getenv("CSRF_TRUSTED_ORIGINS", "").split(',')
 
 
 # Authentication password validators
